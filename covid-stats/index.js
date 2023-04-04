2ee7983884966d7bfc83ed9c24b0e7370e0a80ce
Karthik k <karthik.chinna7399@gmail.com>
2023-04-04 19:46:01 +0530

    covidstats changes

---
+++ covid-stats/index.js
@@ -0,0 +1,21 @@
+//Note install dotenv, nodemon, express and mongoose using (npm i nodemon express mongoose dotenv) before running this
+//this dot env shoud be on top
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+// const {MONGO_URL} = require("./key")
+const PORT = 8001;
+
+const app = express();
+app.use(express.json());
+require("./sendDatatoDb/sendData");
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.on("connected", () => {
+  console.log("connected with mongodb");
+});
+
+app.use(require("./sendDatatoDb/sendData"));
+
+app.listen(PORT, () => {
+  console.log(`app is listening on ${PORT}`);
+});
